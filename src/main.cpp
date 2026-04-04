c2cca3e5a3d54200946fd436117a19bac2d2ad6d
Royyan Dzakiy <r.dzakiy@lmesh.eu>
2026-04-04 22:41:45 +0700

    add workflow

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
-#include <fmt/core.h>
+// #include <fmt/core.h>
 #include <iostream>
 
 auto main() -> int {
-	fmt::println("hello guys!");
+	// fmt::println("hello guys!");
 	std::cout << "test";
+
+	// int *ptr = nullptr;
+	// *ptr = 42;
+	// std::cout << "Value: " << *ptr << std::endl;
 
 	return 0;
 }
