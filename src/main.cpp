708f72c9c21107199692acb3846edaf849c57a8f
Royyan Dzakiy <r.dzakiy@lmesh.eu>
2026-04-04 23:37:18 +0700

    adding fmt print

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
-// #include <fmt/core.h>
+#include <fmt/core.h>
 #include <iostream>
 
 auto main() -> int {
-	// fmt::println("hello guys!");
+	fmt::println("hello guys!");
 	std::cout << "test";
 
-	// int *ptr = nullptr;
-	// *ptr = 42;
-	// std::cout << "Value: " << *ptr << std::endl;
+	int *ptr = nullptr;
+	*ptr = 42;
+	std::cout << "Value: " << *ptr << std::endl;
 
 	return 0;
 }
