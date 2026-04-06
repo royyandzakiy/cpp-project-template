5d9456bded04c0ac4fbba6c0ba4c0c2ad5e7e043
royyandzakiy <royyan.abdul@gmail.com>
2026-04-06 14:02:30 +0700

    feat: add math

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
+#include "math.hpp"
 #include <fmt/core.h>
 
 auto main() -> int {
-	fmt::println("Hello");
+	fmt::println("Add: {}", add(5, 3));
+	fmt::println("Subtract: {}", subtract(10, 4));
+	fmt::println("Multiply: {}", multiply(6, 7));
+	fmt::println("Divide: {}", divide(15, 3));
+	fmt::println("Divide by zero: {}", divide(10, 0));
 
 	return 0;
 }
