208a40e217fc96b03b1651486ede9279f4b7df2b
Royyan Dzakiy <r.dzakiy@lmesh.eu>
2026-06-23 00:54:30 +0700

    init

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,6 @@
-#include "math.hpp"
-#include <fmt/core.h>
+#include <iostream>
 
-auto main() -> int {
-	fmt::println("Add: {}", add(5, 3));
-	fmt::println("Subtract: {}", subtract(10, 4));
-	fmt::println("Multiply: {}", multiply(6, 7));
-	fmt::println("Divide: {}", divide(15, 3));
-	fmt::println("Divide by zero: {}", divide(10, 0));
-
-	return 0;
+int main() {
+    std::cout << "Hello, clangd + CMake!\n";
+    return 0;
 }
