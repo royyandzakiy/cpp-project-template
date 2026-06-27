a24f3b8eea623f6fd1e50ccc2ff099aa24db2e9a
Royyan Dzakiy <r.dzakiy@lmesh.eu>
2026-06-27 09:02:38 +0700

    adding sml

--- project_dependencies.cmake
+++ project_dependencies.cmake
@@ -1,8 +1,3 @@
 find_package(fmt CONFIG REQUIRED)
 find_package(scn CONFIG REQUIRED)
-include(FetchContent)
-FetchContent_Declare(
-  sml
-  GIT_REPOSITORY https://github.com/boost-ext/sml.git
-  GIT_TAG v1.1.13)
-FetchContent_MakeAvailable(sml)
+find_package(sml REQUIRED)
