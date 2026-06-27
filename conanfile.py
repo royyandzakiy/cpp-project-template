dee4693bbaa88b06929646708dd2fa5e33db45ab
Royyan Dzakiy <r.dzakiy@lmesh.eu>
2026-06-27 11:57:32 +0700

    updating conanfile & vcpkg lib version

---
+++ conanfile.py
@@ -0,0 +1,11 @@
+from conan import ConanFile
+
+class MyProjectConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps"
+
+    def requirements(self):
+        self.requires("fmt/12.1.0")
+        self.requires("scnlib/4.0.1")
+        self.requires("tracy/0.13.1")
+        self.requires("sml/1.1.13")
