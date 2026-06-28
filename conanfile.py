31e82432baba91cebeb13a2df3f3e266089be4cd
Royyan Dzakiy <r.dzakiy@lmesh.eu>
2026-06-28 22:38:57 +0700

    gtest from find_package

--- conanfile.py
+++ conanfile.py
@@ -3,6 +3,7 @@ from conan import ConanFile
 class MyProjectConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeDeps"
+    # generators = "CMakeConfigDeps"
 
     def requirements(self):
         self.requires("fmt/12.1.0")
